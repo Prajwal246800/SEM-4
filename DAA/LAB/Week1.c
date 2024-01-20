63c64ac04fcca55e46648890be1e3762cd2cdb1c
Prajwal246800 <ankithgowda24680@gmail.com>
2024-01-20 21:00:13 +0530

    Create Week1.c

---
+++ DAA/LAB/Week1.c
@@ -0,0 +1,16 @@
+/*
+    Aaryan is in a treasure hunt game, there is a digital lock whose password is the single digit which is sum of digits of a number. If the sum of digits exceeds 9 then that new number’s digits sum is the password.To help Aaryan write a recursive C program to find the sum of digits of a number N.
+    Input Format
+        1st line – N an integer
+    Constraints
+        N is positive Integer
+    Output Format
+        Single Integer
+
+    Sample Input 0
+        23
+    Sample Output 0
+        5
+    Explanation 0
+        2 + 3 = 5
+*/
