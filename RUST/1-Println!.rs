f886c06ff91b2e3c3c050815caad146587385a3f
Prajwal246800 <ankithgowda24680@gmail.com>
2024-03-05 17:54:12 +0530

    Create 1-Println!.rs

---
+++ RUST/1-Println!.rs
@@ -0,0 +1,14 @@
+fn main()
+{
+    println!();
+    println!("Hello Ankith!...");
+    println!("format {} arguments", "some");
+}
+
+
+/*
+    OUTPUT:
+    
+    Hello Ankith!...
+    format some arguments
+*/
