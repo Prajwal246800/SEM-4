6e0338c9298b6d7fdd0c5a9ee3363c28d20db06c
Prajwal246800 <ankithgowda24680@gmail.com>
2024-03-05 17:54:20 +0530

    Create 3-variables.rs

---
+++ RUST/3-variables.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let str = "Hello Bhai";
+    let flo = 2.2;
+    let bool = false;
+    let icon = '😂'; //unicode character type
+    
+    println!("String Example : {}", str);
+    println!("Floating Point Example : {}", flo);
+    println!("Boolean Example : {}", bool);
+    println!("Icon Example : {}", icon);
+}
+
+/*
+    OUTPUT:
+    String Example : Hello Bhai
+    Floating Point Example : 2.2
+    Boolean Example : false
+    Icon Example : 😂
+*/
