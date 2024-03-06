3fa09527e87af663c3e6a03ab3349e1ea89d6203
Prajwal246800 <ankithgowda24680@gmail.com>
2024-03-06 11:36:57 +0530

    Create 8-NumberSeperator.rs

---
+++ RUST/8-NumberSeperator.rs
@@ -0,0 +1,14 @@
+fn main()
+{
+    let flo1 = 11_000.555_001;
+    println!("float value {}",flo1);
+    
+    let int1 = 50_000;
+    println!("int value {}",int1);
+}
+
+/*
+    OUTPUT:
+    float value 11000.555001
+    int value 50000
+*/
