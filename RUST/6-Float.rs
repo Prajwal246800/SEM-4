b929c924a491c81d3c457173a59286448a2b9c88
Prajwal246800 <ankithgowda24680@gmail.com>
2024-03-06 11:36:47 +0530

    Create 6-Float.rs

---
+++ RUST/6-Float.rs
@@ -0,0 +1,16 @@
+fn main()
+{
+    let flo1 = 10.00; //f64 by default
+    let flo2:f32 = 8.35;
+    let flo3:f64 = 15000.600; //double precision
+    println!("flo1 is {}",flo1);
+    println!("flo2 is {}",flo2);
+    println!("flo3 is {}",flo3);
+}
+
+/*
+    OUTPUT:
+    flo1 is 10
+    flo2 is 8.35
+    flo3 is 15000.6
+*/
