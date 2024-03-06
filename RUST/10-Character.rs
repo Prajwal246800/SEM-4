9ac91b9e7072eff38e6b1651d4c1a6378b6c7119
Prajwal246800 <ankithgowda24680@gmail.com>
2024-03-06 11:37:55 +0530

    Create 10-Character.rs

---
+++ RUST/10-Character.rs
@@ -0,0 +1,16 @@
+fn main()
+{
+    let char1 = '@'; //default
+    let char2:char = 'A';
+    let char3:char = '😁';
+    println!("char1 is {}",char1);
+    println!("char2 is {}",char2);
+    println!("char3 is {}",char3);
+}
+
+/*
+    OUTPUT:
+    char1 is @
+    char2 is A
+    char3 is 😁
+*/
